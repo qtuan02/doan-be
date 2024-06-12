f62842cf0bc038dc0c1237ed6a467b747c1501da
qtuan02 <quoctuan200702@gmail.com>
2024-06-12 15:32:04 +0700

    main

--- src/main.js
+++ src/main.js
@@ -1,39 +1,21 @@
 const express = require("express");
-const { Category,Brand,Product,Cart,Customer,Order,OrderDetails,Staff,Role } = require("./configs/models");
+const { Banner, Category, Brand, Product, Cart, User, Order, OrderDetail, Favorite } = require("./configs/models");
 const appConfig = require("./configs/env.config");
 const sequelize = require("./configs/connection");
 const cors = require("cors");
 const limiter = require("./common/ratelimit/RateLimit");
 const app = express();
-const adminRoutes = require("./routes/admin/index");
-const userRoutes = require("./routes/customer/index");
-const uploadImage = require("./routes/UploadRoutes")
 
 app.use(cors());
 app.use(express.json());
 app.use(limiter);
 
-app.use('/v1', adminRoutes);
-app.use('/v2', userRoutes);
-app.use('/v3', uploadImage);
-
-app.listen(appConfig.PORT, () => {
+app.listen(appConfig.PORT, async () => {
   console.log(`Running port ${appConfig.PORT}.`);
-
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log("Connected successfully.");
-    })
-    .catch((error) => {
-      console.error("Failed to connect", error);
-    });
-  sequelize
-    .sync()
-    .then(() => {
-      console.log("Created all table successfully.");
-    })
-    .catch((error) => {
-      console.error("Failed to create table", error);
-    });
+  try {
+    await sequelize.sync();
+    console.log('Create all table success.');
+  } catch (error) {
+    console.error('Failed to create table:', error);
+  }
 });
--- src/routes/UploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const uploads = require("../controllers/UploadController");
-const uploadCloud = require("../configs/cloudinary");
-
-const uploadRoutes = Router();
-
-uploadRoutes.post('/uploads', uploadCloud.single('image'), uploads.uploadImage);
-
-module.exports = uploadRoutes;
